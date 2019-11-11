692e8ab1092687acf5689ae884560a51d8e09c94
Flybring <henrik@barnstenit.se>
2019-11-11 16:08:43 +0100

    Added support for GS1 UCC

--- src/PdfSharp/Drawing.BarCodes/Code128.cs
+++ src/PdfSharp/Drawing.BarCodes/Code128.cs
@@ -1,32 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
-
+using System.Text.RegularExpressions;
 using PdfSharp.Drawing;
 using PdfSharp.Drawing.BarCodes;
 
 namespace PdfSharp.Drawing.BarCodes
 {
     /// <summary>A Class to be able to render a Code 128 bar code</summary>
-    /// <remarks>For a more detailed explanation of the Code 128, please visit the following 
-    /// web site: http://www.barcodeman.com/info/c128.php3
-    /// or
+    /// <remarks>For a more detailed explanation of the Code 128, please visit the following web sites:
+    /// http://www.barcodeman.com/info/c128.php3
     /// http://www.adams1.com/128code.html 
+    /// https://sv.wikipedia.org/wiki/Code_128
+    /// 
+    /// ASCII:
+    /// http://www.asciitable.com/
+    /// https://www.asciitabell.se/
     /// </remarks>
     public class Code128 : BarCode
     {
         /// <summary>A static place holder for the patterns to draw the code 128 barcode</summary>
         public static Dictionary<byte, byte[]> Patterns = null;
         private Code128CodeType Code128Code = Code128CodeType.CodeB;
-        private byte[] Values = null;
-        private const int CODE128_STOPCODE = 106;
+        private string BarCode = string.Empty;
+        private const char CodeC = (char)99;
+        private const char CodeB = (char)100;
+        private const char CodeA = (char)101;
+        private const char FNC1 = (char)102;
+        private const int STOPCODE = 106;
 
         /// <summary>Constructor</summary>
         /// <param name="text">String - The text to be coded</param>
         /// <param name="size">XSize - The size of the bar code</param>
         /// <param name="direction">CodeDirection - Indicates the direction to draw the bar code</param>
         public Code128(string text, XSize size, CodeDirection direction)
-        : this(text, size, direction, Code128CodeType.CodeB)
+        : this(text, size, direction, Code128CodeType.CodeB, false)
+        {
+        }
+
+        /// <summary>Constructor</summary>
+        /// <param name="text">String - The text to be coded</param>
+        /// <param name="size">XSize - The size of the bar code</param>
+        /// <param name="direction">CodeDirection - Indicates the direction to draw the bar code</param>
+        /// <param name="isUCC">Set barcode to GS1 EAN/UCC</param>
+        public Code128(string text, XSize size, CodeDirection direction, bool isUCC)
+        : this(text, size, direction, Code128CodeType.Auto, isUCC)
+        {
+        }
+
+        /// <summary>Constructor</summary>
+        /// <param name="text">String - The text to be coded</param>
+        /// <param name="size">XSize - The size of the bar code</param>
+        /// <param name="direction">CodeDirection - Indicates the direction to draw the bar code</param>
+        /// <param name="code128Code">Code_128_Code_Types - Indicates which of the codes to use when rendering the bar code.
+        public Code128(string text, XSize size, CodeDirection direction, Code128CodeType code128Code)
+        : this(text, size, direction, code128Code, false)
         {
         }
 
@@ -36,81 +65,23 @@ namespace PdfSharp.Drawing.BarCodes
         /// <param name="direction">CodeDirection - Indicates the direction to draw the bar code</param>
         /// <param name="code128Code">Code_128_Code_Types - Indicates which of the codes to use when rendering the bar code.
         /// The options are A, B, or buffer.</param>
-        public Code128(string text, XSize size, CodeDirection direction, Code128CodeType code128Code)
+        public Code128(string text, XSize size, CodeDirection direction, Code128CodeType code128Code, bool isUCC)
         : this(null, text, size, direction, code128Code)
         {
             this.Text = text;
+            text = isUCC ? PrepareGS1Text(text) : text;
 
-            // Create the array to hold the values to be rendered
             if (Code128Code == Code128CodeType.CodeC)
             {
                 // Ensure that the text is an even length
                 //if ((text.Length % 2) == 1) throw new ArgumentOutOfRangeException("Parameter text (string) must have an even length for Code 128 - Code C");
                 if ((text.Length % 2) == 1)
                     text = "0" + text;
-
-                Values = new byte[text.Length / 2];
-            }
-            else
-            {
-                Values = new byte[text.Length];
             }
 
+            BarCode = ParseTextToBarCode(text);
 
-            string buffer = "";
-            for (int x = 0; x < text.Length; x++)
-            {
-                switch (Code128Code)
-                {
-                    case Code128CodeType.CodeA:
-                        if (text[x] < 32)
-                        {
-                            Values[x] = (byte)(text[x] + 64);
-                        }
-                        else if ((text[x] >= 32) && (text[x] < 64))
-                        {
-                            Values[x] = (byte)(text[x] - 32);
-                        }
-                        else
-                        {
-                            Values[x] = (byte)text[x];
-                        }
-                        break;
-
-                    case Code128CodeType.CodeB:
-                        Values[x] = (byte)(text[x] - 32);
-                        break;
-
-                    case Code128CodeType.CodeC:
-                        if ((text[x] >= '0') && (text[x] <= '9'))
-                        {
-                            buffer += text[x];
-                            if (buffer.Length == 2)
-                            {
-                                Values[x / 2] = byte.Parse(buffer);
-                                buffer = "";
-                            }
-                        }
-                        else
-                        {
-                            throw new ArgumentOutOfRangeException("Parameter text (string) can only contain numeric characters for Code 128 - Code C");
-                        }
-                        break;
-                }
-            }
             CheckValues();
-        }
-
-
-        private void CheckValues()
-        {
-            if (Values == null) throw new InvalidOperationException("Text or Values must be set");
-            if (Values.Length == 0) throw new InvalidOperationException("Text or Values must have content");
-
-            for (int x = 0; x < Values.Length; x++)
-            {
-                if (Values[x] > 102) throw new ArgumentOutOfRangeException(BcgSR.InvalidCode128(x));
-            }
         }
 
         /// <summary>Constructor</summary>
@@ -126,7 +97,201 @@ namespace PdfSharp.Drawing.BarCodes
             if (!Enum.IsDefined(typeof(Code128CodeType), code128Code)) throw new ArgumentOutOfRangeException("Parameter code128Code (Code_128_Code_Types) is invalid");
             if (Patterns == null) Load();
             Code128Code = code128Code;
-            Values = values;
+            //Values = values;
+        }
+
+        /// <summary>
+        /// TODO: Validate Application Identifiers (AI)
+        /// Parse text to raw data with start codes. Optimizes data if possible.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string ParseTextToBarCode(string text)
+        {
+            Code128CodeType startCode = GetStartCode(text, Code128Code);
+            Code128CodeType currentCode = startCode;
+
+            string values = string.Empty;
+            values += (char)startCode;
+
+            int index = 0;
+            while (index < text.Length)
+            {
+                if (Code128Code == Code128CodeType.Auto
+                    && (currentCode == Code128CodeType.CodeA || currentCode == Code128CodeType.CodeB)
+                    && GetNumberCount(text, index) >= 4)
+                {
+                    currentCode = Code128CodeType.CodeC;
+                    values += CodeC;
+                    continue;
+                }
+
+                int charNumber = text[index];
+                if (charNumber == FNC1)
+                {
+                    values += FNC1;
+                    index++;
+                    continue;
+                }
+
+                if (currentCode == Code128CodeType.CodeA)
+                {
+                    if (charNumber < 32)
+                        values += (char)(charNumber + 64);
+                    else if (charNumber >= 95)
+                    {
+                        currentCode = Code128CodeType.CodeB;
+                        values += CodeB;
+                        values += (char)(charNumber - 32);
+                    }
+                    else
+                        values += (char)(charNumber - 32);
+
+                    index++;
+                }
+                else if (currentCode == Code128CodeType.CodeB)
+                {
+                    if (IsCharAType(charNumber))
+                    {
+                        currentCode = Code128CodeType.CodeA;
+                        values += CodeA;
+                        values += (char)(charNumber + 64);
+                    }
+                    else
+                        values += (char)(charNumber - 32);
+
+                    index++;
+                }
+                else if (currentCode == Code128CodeType.CodeC)
+                {
+                    if (GetNumberCount(text, index) >= 2)
+                    {
+                        var valuePair = GetNumberPairChar(text, index);
+                        values += valuePair.Value;
+                        index += valuePair.Key;
+                    }
+                    else
+                    {
+                        if (IsCharAType(charNumber))
+                        {
+                            currentCode = Code128CodeType.CodeA;
+                            values += CodeA;
+                            values += (char)(charNumber + 64);
+                        }
+                        else
+                        {
+                            currentCode = Code128CodeType.CodeB;
+                            values += CodeB;
+                            values += (char)(charNumber - 32);
+                        }
+
+                        index++;
+                    }
+                }
+
+                if (Code128Code != Code128CodeType.Auto && currentCode != startCode)
+                    throw new Exception($"Invalid characters in barcode for CodeType at index {index}");
+            }
+
+            return values.ToString();
+        }
+
+        private bool IsCharAType(int charNumber) => charNumber < 32;
+
+        /// <summary>
+        /// Add FNC1 and remove unwanted characters
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string PrepareGS1Text(string text)
+        {
+            Regex regex = new Regex("[^a-zA-Z0-9( -]");
+            text = regex.Replace(text, "");
+            text = text.Replace('(', FNC1);
+            return text;
+        }
+
+        /// <summary>
+        /// Get optimized char from value
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        private KeyValuePair<int, string> GetNumberPairChar(string inputText, int startIndex)
+        {
+            var values = new StringBuilder();
+            string text = inputText.Substring(startIndex);
+
+            int index = 0;
+            if (text.StartsWith(FNC1.ToString()))
+            {
+                values.Append(FNC1);
+                index++;
+            }
+
+            //ASCII 0 = 48, 9 = 57
+            int firstChar = text[index++] - 48;
+            int secondChar = text[index++] - 48;
+
+            char optimizedChar = (char)(firstChar * 10 + secondChar);
+            values.Append(optimizedChar);
+
+            return new KeyValuePair<int, string>(index, values.ToString());
+        }
+
+        /// <summary>
+        /// Get start code
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private Code128CodeType GetStartCode(string text, Code128CodeType type)
+        {
+            if (type == Code128CodeType.Auto && GetNumberCount(text, 0) >= 2)
+                return Code128CodeType.CodeC;
+            else if (type == Code128CodeType.Auto)
+            {
+                int firstValue = text[0];
+                if (firstValue < 32)
+                    return Code128CodeType.CodeA;
+                else
+                    return Code128CodeType.CodeB;
+            }
+
+            return type;
+        }
+
+        /// <summary>
+        /// Get number count in text, ignores FNC1
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        private int GetNumberCount(string text, int startIndex)
+        {
+            int count = 0;
+            foreach (var character in text.Substring(startIndex))
+            {
+                if (character == FNC1)
+                    continue;
+                else if (char.IsNumber(character))
+                    count++;
+                else
+                    break;
+            }
+
+            return count;
+        }
+
+        private void CheckValues()
+        {
+            if (BarCode == null) throw new InvalidOperationException("Text or Values must be set");
+            if (BarCode.Length == 0) throw new InvalidOperationException("Text or Values must have content");
+
+            for (int x = 0; x < BarCode.Length; x++)
+            {
+                var charNumber = BarCode[x];
+                if (charNumber > 127 && charNumber != FNC1) throw new ArgumentOutOfRangeException(BcgSR.InvalidCode128(x));
+            }
         }
 
         /// <summary>Creates a new instance of the Patterns field and populates it with the appropriate
@@ -265,26 +430,19 @@ namespace PdfSharp.Drawing.BarCodes
             info.CurrPosInString = 0;
             info.CurrPos = position - CodeBase.CalcDistance(AnchorType.TopLeft, this.Anchor, this.Size);
 
-            RenderStart(info);
-            foreach (byte c in Values)
-            {
-                RenderValue(info, (int)c);
-            }
+            foreach (char c in BarCode)
+                RenderValue(info, c);
+
             RenderStop(info);
             if (TextLocation != TextLocation.None) RenderText(info);
 
             gfx.Restore(state);
         }
 
-        private void RenderStart(BarCodeRenderInfo info)
-        {
-            RenderValue(info, (int)Code128Code);
-        }
-
         private void RenderStop(BarCodeRenderInfo info)
         {
             RenderValue(info, CalculateParity());
-            RenderValue(info, CODE128_STOPCODE);
+            RenderValue(info, STOPCODE);
         }
 
         private void RenderValue(BarCodeRenderInfo info, int chVal)
@@ -348,10 +506,10 @@ namespace PdfSharp.Drawing.BarCodes
 
         private int CalculateParity()
         {
-            long parityValue = (int)Code128Code;
-            for (int x = 1; x <= Values.Length; x++)
+            long parityValue = BarCode[0];
+            for (int x = 1; x < BarCode.Length; x++)
             {
-                parityValue += ((Values[x - 1]) * x);
+                parityValue += (BarCode[x] * x);
             }
             parityValue %= 103;
             return (int)parityValue;
@@ -395,10 +553,10 @@ namespace PdfSharp.Drawing.BarCodes
 
         internal override void InitRendering(BarCodeRenderInfo info)
         {
-            if (Values == null) throw new InvalidOperationException(BcgSR.BarCodeNotSet);
-            if (Values.Length == 0) throw new InvalidOperationException(BcgSR.EmptyBarCodeSize);
+            if (BarCode == null) throw new InvalidOperationException(BcgSR.BarCodeNotSet);
+            if (BarCode.Length == 0) throw new InvalidOperationException(BcgSR.EmptyBarCodeSize);
 
-            int numberOfBars = Values.Length + 3; // The length of the string plus the start, stop, and parity value
+            int numberOfBars = BarCode.Length + 2; // The length of the string stop, and parity value
             numberOfBars *= 11; // Each character has 11 bars
             numberOfBars += 2; // Add two more because the stop bit has two extra bars
 
@@ -411,6 +569,8 @@ namespace PdfSharp.Drawing.BarCodes
     /// <summary>Code types for Code 128 bar code</summary>
     public enum Code128CodeType
     {
+        /// <summary>Optimize code based on content</summary>
+        Auto = 0,
         /// <summary>Code A</summary>
         CodeA = 103,
         /// <summary>Code B</summary>
