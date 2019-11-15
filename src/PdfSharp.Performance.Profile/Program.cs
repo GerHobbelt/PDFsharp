05b0029e24aef056edfbf8dee1650fde467f9da5
Ger Hobbelt <ger@hobbelt.com>
2019-11-15 18:54:44 +0100

    pick up test projects from SHA-1: 3ede991e1eaa35596ec4e526021b232feb4e6625 / Jamin Menter

---
+++ src/PdfSharp.Performance.Profile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using PdfSharp.Pdf.IO;
+
+namespace PdfSharp.Performance.Profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Reading {args.Length} files...");
+            foreach (var path in args)
+            {
+                if (File.Exists(path))
+                {
+                    Console.Write($"Reading {path}...");
+                    var pdf = PdfReader.Open(new FileStream(path, FileMode.Open), PdfDocumentOpenMode.Modify);
+                    Console.WriteLine($" Complete! {pdf.PageCount} pages read.");
+                }
+                else
+                    Console.Error.WriteLine("File not found:  " + path);
+            }
+#if DEBUG
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
